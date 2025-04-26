bfc60faf73ccd045c3e750f06a5c57dddd802370
Keerthiya T <keerthithirumurthy1@gmail.com>
2025-04-26 13:53:45 +0530

    Create Solution9.java
    Input Format: N = 6
    Result:   
    A B C D E F
    A B C D E 
    A B C D
    A B C
    A B
    A

---
+++ Patterns/Solution9.java
@@ -0,0 +1,17 @@
+
+class Main {
+   
+   static void pattern15(int N)
+{
+     
+      for(int i=0;i<N;i++){
+          
+          for(char ch = 'A'; ch<='A'+(N-i-1);ch++){
+              System.out.print(ch + " ");
+              
+          }
+        
+          System.out.println();
+         
+      }
+}
